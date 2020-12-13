3bcc505563b7695ec3b787de7bae2368a6aca3ff
potatoyum <46017398+potatoyum@users.noreply.github.com>
2020-12-14 04:21:04 +0900

    데이터 읽기
    xlsx 사용하여 db에 저장

--- server/models/lip_reading.js
+++ server/models/lip_reading.js
@@ -2,8 +2,12 @@ var mongoose=require('mongoose');
 var Schema= mongoose.Schema;
 
 var lip_readingSchema = new Schema({
-start: 'string',
-
+    id: 'number',
+    link: 'string',
+    start: 'string',
+    end: 'string',
+    answer: 'string',
+    type: 'string' // sentence or word
 } );
 
 module.exports = mongoose.model('lip_reading', lip_readingSchema);
