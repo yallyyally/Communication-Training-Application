983a1394201d7c7df45877b7810e7d83baea7768
potatoyum <46017398+potatoyum@users.noreply.github.com>
2020-12-14 03:09:11 +0900

    스키마모델, api수정
    get 메소드 정상 작동

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); //환경 변수
 const express= require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -27,14 +28,19 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Successfully connected to mongodb'))
     .catch(e => console.error(e));
 
+
 //define model
 var Lip_reading= require('./models/lip_reading');
 
+//독화훈련 데이터 get
 app.get('/api/lip_reading',(req,res)=>{
 
-    db.lip_reading.find(function(err,lip_readings){
+    Lip_reading.find(function(err,lip_readings){
         if(err) return res.status(500).json({error:err});
+        if(!lip_readings) console.log("aaa");
+        console.log(lip_readings);
         res.json(lip_readings);
     })
 
 })
+
