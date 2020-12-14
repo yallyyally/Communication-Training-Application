bd1d38dadf861030e5be477c4b7900e40d2b206a
yallyyally <jhshin0028@gmail.com>
2020-12-15 07:11:22 +0900

    동영상 구간 설정, 하단바 삭제
    버튼 클릭 이벤트, 반복재생 추가 필요

--- CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/YoutubeSampleActivity.java
+++ CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/YoutubeSampleActivity.java
@@ -18,6 +18,13 @@ public class YoutubeSampleActivity extends YouTubeBaseActivity{
     Button btn;
     YouTubePlayer.OnInitializedListener listener;
 
+    String videoPath;
+
+    int starPoint_min;
+    int startPoint_sec;
+
+    int startPoint_mili;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +33,18 @@ public class YoutubeSampleActivity extends YouTubeBaseActivity{
         btn = findViewById(R.id.youtubeBtn);
         youTubePlayerView = findViewById(R.id.youtubeView);
 
+        starPoint_min = 1;
+        startPoint_sec = 1;
+        startPoint_mili = ((starPoint_min * 60) + startPoint_sec) * 1000;
+        videoPath="2jdNqxerUao";
 
         listener = new YouTubePlayer.OnInitializedListener() {
+
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("NmkYHmiNArc"); //
+
+
+                youTubePlayer.loadVideo(videoPath, startPoint_mili); //1분 1초부터 시작
                 youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);
                 //https://www.youtube.com/watch?v=NmkYHmiNArc 유투브에서 v="" 이부분이 키에 해당
 
@@ -45,11 +59,21 @@ public class YoutubeSampleActivity extends YouTubeBaseActivity{
 
 
         };
+        youTubePlayerView.initialize("아무키", listener);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"다음 문제",Toast.LENGTH_SHORT).show();
+
+                //버튼 누르면 동영상 변경
+                videoPath = "NmkYHmiNArc";
+
+                 starPoint_min = 1;
+                 startPoint_sec = 30;
+
+                 startPoint_mili = ((starPoint_min * 60) + startPoint_sec) * 1000;
+
                 youTubePlayerView.initialize("아무키", listener);
 
             }
