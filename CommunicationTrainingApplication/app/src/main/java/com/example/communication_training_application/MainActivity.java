6cc7b41ec51244dab8a39bb6afe968336a1d7af4
YeongwonHam <yeomgg012@gmail.com>
2020-12-15 02:48:43 +0900

    딕셔너리 액티비티 추가/ 잡다 버튼 연결

--- CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/MainActivity.java
+++ CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.communication_training_application;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLip = (Button) findViewById(R.id.btn_lip);
+        btnLip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LipReadingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnDic = (Button) findViewById(R.id.btn_dic);
+        btnDic.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),DictionaryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
