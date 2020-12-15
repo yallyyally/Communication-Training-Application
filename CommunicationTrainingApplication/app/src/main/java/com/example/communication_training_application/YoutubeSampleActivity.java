6302fbc5333f186d303f9dfeda46aa510097f590
yallyyally <jhshin0028@gmail.com>
2020-12-15 19:41:23 +0900

    재생 시작 시간, 재생 종료 시간 설정
    버튼 클릭 이벤트 처리 해줘야 함
    종료 후, 이전시간으로 가서 다시 play 해줘야 함,,,,

--- CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/YoutubeSampleActivity.java
+++ CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/YoutubeSampleActivity.java
@@ -3,8 +3,10 @@ package com.example.communication_training_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -18,29 +20,37 @@ public class YoutubeSampleActivity extends YouTubeBaseActivity {
     private YouTubePlayer.OnInitializedListener mOnInitializedListener;
     private YouTubePlayer mYouTubePlayer;
     private Button mButton;
-
+    private TextView text;
     private String VIDEO_CODE;
     private final String VIDEO_CODE_ORG = "-Uf2YC1YBuI";
     private final String API_KEY = "apikey";
+
+    public int endTime ;
+    private int startPoint_mili;
+    private int startPoint_min;
+    private int startPoint_sec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_sample);
         youTubePlayerSetup();
+
         mButton = findViewById(R.id.Button_Next);
+        text = findViewById(R.id.TextView_Time);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //시작 시간 설정
-                int starPoint_min = 1;
-                int startPoint_sec = 1;
-                int startPoint_mili = ((starPoint_min * 60) + startPoint_sec) * 1000;
+                startPoint_min = 1;
+                startPoint_sec = 1;
+                startPoint_mili = ((startPoint_min * 60) + startPoint_sec) * 1000;
+                endTime = startPoint_mili + 6500;
 
                 //동영상 설정
-                VIDEO_CODE= "2jdNqxerUao";
+                VIDEO_CODE = "2jdNqxerUao";
 
                 mYouTubePlayer.loadVideo(VIDEO_CODE, startPoint_mili); //1분 1초부터 시작
 
@@ -50,6 +60,27 @@ public class YoutubeSampleActivity extends YouTubeBaseActivity {
 
             }
         });
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //For every 1 second, check the current time and endTime
+                if (mYouTubePlayer.getCurrentTimeMillis() <= endTime) {
+                    text.setText("Video Playing at " + mYouTubePlayer.getCurrentTimeMillis());
+                    handler.postDelayed(this, 1000);
+                } else {
+                    handler.removeCallbacks(this); //no longer required
+                    text.setText(" Reached " + endTime);
+                    mYouTubePlayer.pause(); //and Pause the video
+                    //and restart
+                    //mYouTubePlayer.loadVideo(VIDEO_CODE, startPoint_mili); //1분 1초부터 시작
+
+
+                }
+            }
+        }, 1000);
     }
 
     //초기 설정
@@ -69,7 +100,7 @@ public class YoutubeSampleActivity extends YouTubeBaseActivity {
                     int starPoint_min_org = 1;
                     int startPoint_sec_org = 30;
                     int startPoint_mili_org = ((starPoint_min_org * 60) + startPoint_sec_org) * 1000;
-
+                    endTime = startPoint_mili_org + 6500; //종료시간 설정, 6.5초 재생 가정..
                     mYouTubePlayer.loadVideo(VIDEO_CODE_ORG, startPoint_mili_org); //1분 1초부터 시작
 
                     //하단바 안보이게
