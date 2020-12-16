6a3cbb8b971ef41d7f7aee085882876f47b95ffc
potatoyum <46017398+potatoyum@users.noreply.github.com>
2020-12-17 05:18:42 +0900

    설명 슬라이더 구현
    뷰페이저 활용

--- CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/MainActivity.java
+++ CommunicationTrainingApplication/app/src/main/java/com/example/communication_training_application/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         btnLip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LipReadingActivity.class);
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
                 startActivity(intent);
             }
         });
